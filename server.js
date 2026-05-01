9dc73051337b096fd7c75a960e8a4cc1e98cd43c
gionny78 <giodido1978@tiscali.it>
2026-05-02 01:24:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -17,16 +17,15 @@ app.get("/matches", async (req, res) => {
         method: "GET",
         headers: {
           "X-RapidAPI-Key": process.env.API_KEY,
-          "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
-        },
+          "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
+        }
       }
     );
 
     const data = await response.json();
-
     res.json(data);
+
   } catch (error) {
-    console.error(error);
     res.send("Errore API: " + error.message);
   }
 });
