dbe5f92e04b689d40dea0232cfdf3b5485a0a0d5
gionny78 <giodido1978@tiscali.it>
2026-05-03 11:30:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,28 +5,25 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 📌 SERVE SOLO FILE STATICI (index.html)
-app.use(express.static(path.join(__dirname)));
+// serve index.html
+app.use(express.static(__dirname));
 
-// 📌 HOMEPAGE FORZATA (QUESTO RISOLVE IL TUO BUG)
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// 📌 API PARTITE
+// API
 app.get("/matches", async (req, res) => {
   try {
     const response = await fetch("https://www.scorebat.com/video-api/v3/");
     const data = await response.json();
-
     res.json(data.response);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: "Errore nel server" });
+    res.status(500).json({ error: "Errore server" });
   }
 });
 
-// 📌 START SERVER
 app.listen(PORT, () => {
   console.log("Server attivo sulla porta " + PORT);
 });
